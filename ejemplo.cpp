9272ea5bb8af3b820127702f37952bf6ab417a6a
JosueMaquera <jmaquerap@unjbg.edu.pe>
2025-06-18 22:06:57 -0500

    Mostrar empleado con mayores ventas anuales

--- ejemplo.cpp
+++ ejemplo.cpp
@@ -1,50 +1,74 @@
-#include<iostream>
-#include<string>
+#include <iostream>
+#include <string>
 
 using namespace std;
 
-struct EMP {
-    int numero_empleado;
+struct Empleado {
+    int numeroEmpleado;
     string nombre;
     float ventas[12];
     float salario;
+    float ventasTotales;
 };
 
-int main () {
-    
-    int cantidad_empleados;
-    EMP empleados[100];
+int main() {
+    int cantidadEmpleados;
+    int indiceMayorVentas = 0;
+    float mayorVentas;
+
+    Empleado empleados[100];
 
     cout << "Ingresa el numero de empleados a registrar: ";
-    cin >> cantidad_empleados;
+    cin >> cantidadEmpleados;
 
-    for (int i = 0; i < cantidad_empleados; i++) {
+    for (int i = 0; i < cantidadEmpleados; i++) {
+        cout << "\nIngrese los datos del empleado #" << i + 1 << endl;
 
-        cout << "Ingrese su numero de empleado: ";
-        cin >> empleados[i].numero_empleado;
+        cout << "Numero de empleado: ";
+        cin >> empleados[i].numeroEmpleado;
         cin.ignore();
-        cout << "Ingrese su nombre: ";
-        getline(cin, empleados[i].nombre);
-        
-        cout << "Ingrese sus ventas: " << endl;
 
+        cout << "Nombre: ";
+        getline(cin, empleados[i].nombre);
+
+        empleados[i].ventasTotales = 0;
+
+        cout << "Ingrese las ventas de los 12 meses:\n";
         for (int j = 0; j < 12; j++) {
+            cout << "Mes " << j + 1 << ": ";
             cin >> empleados[i].ventas[j];
+            empleados[i].ventasTotales += empleados[i].ventas[j];
         }
-        cout << "Ingrese su salario: ";
+
+        cout << "Salario: ";
         cin >> empleados[i].salario;
     }
 
-    for (int i = 0; i < cantidad_empleados; i++) {
-        cout << "--------Empleado " << i + 1 << "----------" << endl;
-        cout << "Numero de empleado: " << empleados[i].numero_empleado << endl;
+    mayorVentas = empleados[0].ventasTotales;
+
+    for (int i = 0; i < cantidadEmpleados; i++) {
+        if (empleados[i].ventasTotales > mayorVentas) {
+            mayorVentas = empleados[i].ventasTotales;
+            indiceMayorVentas = i;
+        }
+        
+        cout << "\n-------- Empleado #" << i + 1 << " ----------" << endl;
+        cout << "Numero de empleado: " << empleados[i].numeroEmpleado << endl;
         cout << "Nombre: " << empleados[i].nombre << endl;
-        cout << "Ventas: ";
+        cout << "Ventas mensuales: ";
         for (int j = 0; j < 12; j++) {
             cout << empleados[i].ventas[j] << " ";
-            
         }
-        cout << endl;
+        cout << "\nVentas totales: " << empleados[i].ventasTotales << endl;
         cout << "Salario: " << empleados[i].salario << endl;
     }
+
+    cout << "\n-----------------------------------------" << endl;
+    cout << "Empleado con mayor ventas anuales:" << endl;
+    cout << "Numero de empleado: " << empleados[indiceMayorVentas].numeroEmpleado << endl;
+    cout << "Nombre: " << empleados[indiceMayorVentas].nombre << endl;
+    cout << "Ventas totales: " << empleados[indiceMayorVentas].ventasTotales << endl;
+
+    return 0;
 }
+
